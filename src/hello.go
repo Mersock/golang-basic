90fdccc7f3beebd4c80d9627f08134345bc0f25e
Phumthawan Nokun <phumthawan.nok@ascendcorp.com>
2019-09-09 11:07:30 +0700

    add basic variable

--- src/hello.go
+++ src/hello.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Print("hello, world\n");
+func main() {
+	/*
+		Initial
+	*/
+	//fmt.Println("xxxx");
+	//fmt.Print("hello, world\n");
+
+	/*
+		Variable
+	*/
+	//var age int = 200
+	age2 := 5000000.22332
+	text := "This is new text"
+	num1, num2 := 323, 22
+
+	fmt.Println("Variable integer is", age2);
+	fmt.Println("Text common now is", text);
+	fmt.Println("Text number one", num1);
+	fmt.Println("Text number two", num2);
 }
