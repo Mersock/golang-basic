262eaf907110b280f6b300488b3818e388db3a23
Phumthawan Nokun <phumthawan.nok@ascendcorp.com>
2019-09-09 13:34:48 +0700

    number and string

--- src/hello.go
+++ src/hello.go
@@ -13,12 +13,33 @@ func main() {
 		Variable
 	*/
 	//var age int = 200
-	age2 := 5000000.22332
-	text := "This is new text"
-	num1, num2 := 323, 22
+	//age2 := 5000000.22332
+	//text := "This is new text"
+	//num1, num2 := 323, 22
+	//fmt.Println("Variable integer is", age2);
+	//fmt.Println("Text common now is", text);
+	//fmt.Println("Text number one", num1);
+	//fmt.Println("Text number two", num2);
 
-	fmt.Println("Variable integer is", age2);
-	fmt.Println("Text common now is", text);
-	fmt.Println("Text number one", num1);
-	fmt.Println("Text number two", num2);
+	/*
+		number and string
+	*/
+	number1 := 5
+	number2 := 4
+	p1 := "knz"
+	p2 := "phumthawan"
+	//operation
+	fmt.Println(number1 + number2)
+	fmt.Println(number1 - number2)
+	fmt.Println(number1 * number2)
+	fmt.Println(number1 / number2)
+
+	//concat string
+	p3 := p1 + p2
+	fmt.Println(p3)
+	//ascii format
+	fmt.Println(p3[1])
+
+	fmt.Println(p3[0:])
+	fmt.Println(p3[1:3])
 }
